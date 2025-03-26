314fd6d993538a84cd3fe8162c7c2dead1c15dfb
Nicolas <laconis.dev@gmail.com>
2025-03-26 22:26:11 +0100

    Commit de Base

---
+++ GestionFilms/Films.cs
@@ -0,0 +1,23 @@
+﻿class Films
+{
+
+    public string titre = "Inconnu";
+    public long tailleFilms = 0;
+
+    public Films(int id)
+    {
+
+    }
+
+    public Films(string name)
+    {
+
+    }
+
+
+    public static void addFilms(string path)
+    {
+
+    }
+
+}
---
+++ GestionFilms/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+class Start
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(45 << 54 >> 45 );
+
+
+
+
+        
+        Console.WriteLine("Ok");
+        Display.getBanner();
+        Display.getMenu();
+
+    }
+
+
+}
